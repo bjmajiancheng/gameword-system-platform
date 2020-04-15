4f421747aba219b84e26a8bb52ebdc07c2a3a931
majiancheng <majiancheng@davdian.com>
2020-04-15 22:49:26 +0800

    [其他]说明：u [影响]u

---
+++ src/main/java/com/gameword/system/system/service/ILabelService.java
@@ -0,0 +1,34 @@
+/*
+ * 北京果敢时代科技有限公司
+ * 北京市朝阳区望京SOHO T3 B座1607
+ * 邮编：100022
+ * 网址：www.davdian.com
+ */
+
+package com.gameword.system.system.service;
+
+import com.gameword.system.common.baseservice.IBaseService;
+import com.gameword.system.system.model.LabelModel;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import java.util.*;
+import com.gameword.system.system.dao.*;
+import com.gameword.system.system.service.*;
+
+/**
+ * @author  davdian
+ * @version 1.0
+ * @since 1.0
+ */
+
+public interface ILabelService extends IBaseService<LabelModel> {
+
+	public LabelModel findById(Integer id);
+
+	public List<LabelModel> find(Map<String, Object> param);
+
+	public PageInfo<LabelModel> selectByFilterAndPage(LabelModel labelModel, int pageNum, int pageSize);
+
+	public List<LabelModel> selectByFilter(LabelModel labelModel);
+
+}
